7ac13af6b456b1c10241ecf58747a17cdf8c5440
Josue Velasquez <joshvelt05@gmail.com>
2022-08-08 14:34:40 -0600

    categoria 2

--- nutrifitapp/models.py
+++ nutrifitapp/models.py
@@ -6,13 +6,13 @@ from django.urls import reverse_lazy
 # Create your models here.
 
 #class Categoria(models.Model):
-    #nombre = models.CharField(max_length=255)
+#    nombre = models.CharField(max_length=255)
+#
+#    def __str__(self):
+#        return self.nombre
 
-    #def __str__(self):
-        #return self.nombre
-
-    #def get_absolute_url(self):
-        #return reverse_lazy("inicio")
+#    def get_absolute_url(self):
+#        return reverse_lazy("inicio")
 
 
 class Producto(models.Model):
