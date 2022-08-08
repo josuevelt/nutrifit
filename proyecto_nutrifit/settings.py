babfe70dd160078bcae10edeb088271e8b88ea35
Josue Velasquez <joshvelt05@gmail.com>
2022-08-08 14:54:14 -0600

    imagenes

--- proyecto_nutrifit/settings.py
+++ proyecto_nutrifit/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "media")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), os.path.join(BASE_DIR, "media")]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
